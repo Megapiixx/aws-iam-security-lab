20d6a68a517ae5ef967233d922202a52ff79cbcd
Megapiixx <fernando1078593@gmail.com>
2025-04-01 10:52:12 -0400

    Create Readme.d

---
+++ Readme.d
@@ -0,0 +1,16 @@
+# AWS IAM Security Lab
+## 🔹 Overview
+This project demonstrates **IAM security best practices** by implementing **least privilege access, MFA enforcement, and AWS CloudTrail monitoring**.
+
+## 🔹 Steps Implemented
+✅ Created IAM users & groups (Developers & Admins).  
+✅ Applied **least privilege IAM policy**.  
+✅ Enforced **MFA for secure login**.  
+✅ Enabled **CloudTrail to track IAM events**.  
+
+## 🔹 Screenshots
+- IAM Policy: ![IAM Policy](screenshots/iam-policy.png)
+- CloudTrail Log: ![CloudTrail Log](screenshots/cloudtrail-log.png)
+
+## 🔹 Resources
+- AWS IAM Docs: [https://docs.aws.amazon.com/IAM/](https://docs.aws.amazon.com/IAM/)
